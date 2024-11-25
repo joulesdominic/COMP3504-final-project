2c96f46aaa124397cfd48cad428bf9bab8b77671
joulesdominic <joulesdominic@gmail.com>
2024-11-25 00:47:33 -0700

    Added insertion to database table

--- src/routes/generateStory.js
+++ src/routes/generateStory.js
@@ -1,23 +1,44 @@
 import express from 'express';
 import { getGroqChatCompletion } from '../services/generate.js';
 
-const router = express.Router();
+const generateStory = (supabaseClient) => {
+   const router = express.Router();
 
-router.post('/', async (req, res) => {
-   const { name, age, topic, genre } = req.body;
+   router.post('/', async (req, res) => {
+      const { name, age, topic, genre } = req.body;
 
-   if (!name || !age || !topic) {
-    return res.status(400).json({ error: 'Name, age, topic, and genre are required.'});
-   }
+      if (!name || !age || !topic) {  
+       return res.status(400).json({ error: 'Name, age, topic, and genre are required.'});
+      }
 
-   const prompt = `Write a short children's story that includes ${name} as the protagonist, along with their age: ${age}, and based on the given topic: ${topic} and genre: ${genre}.`;
-   try {
-    const chatCompletion = await getGroqChatCompletion(prompt);
+      const prompt = `Write a short children's story that includes ${name} as the protagonist, along with their age: ${age}, and based on the given topic: ${topic} and genre: ${genre}.`;
+      try {
+       const chatCompletion = await getGroqChatCompletion(prompt);
+       
 
-    res.status(200).json(JSON.parse(chatCompletion.choices[0].message.content));
-   } catch (e) {
-    console.log(e);
-   }
-});
+       const story = JSON.parse(chatCompletion.choices[0].message.content);
+       const storyText = story.story.text;
 
-export default router;
+       const { data, error } = await supabaseClient.from('books').insert({
+          title: story.title,
+          child_name: name,
+          child_age: age,
+          topic: topic,
+          genre: genre,
+          story: storyText
+       });
+
+       if (error) {
+         console.error('Error inserting data:', error);
+         res.status(500).json({ error: 'Failed to insert data' });
+       } else {
+         res.status(200).json(story);
+       }
+      } catch (e) {
+       console.log(e);
+      }
+   });
+   return router;
+}
+
+export default generateStory;
